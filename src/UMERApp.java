f91b8e48c98fcb727edc5af21358bac5a8b17de5
Frederico Pinto <frederico21pinto@hotmail.com>
2017-06-02 00:15:22 +0100

    Só quero dizer

--- src/UMERApp.java
+++ src/UMERApp.java
@@ -26,8 +26,7 @@ public class UMERApp{
         initApp(file_name);
         apresentarMenu();
         try {
-            um.gravaObj(file_name);
-            um.log("snap.data", true);
+            um.save();
         }
         catch (IOException e) {
             System.out.println("Não consegui gravar os dados!");
@@ -42,7 +41,7 @@ public class UMERApp{
         int running = 1;
 
         do {
-            if(um.getUtilizador() != null){
+            if(um.getUtilizadorC() != null){
                 menu_logado.executa();
                 switch(menu_logado.getOpcao()){
                     case 1: menu();
@@ -56,7 +55,7 @@ public class UMERApp{
             else{
                 menu_principal.executa();
                 switch (menu_principal.getOpcao()) {
-                    case 1: registo();
+                    case 1: registarUtilizador();
                             break;
                     case 2: iniciarSessao();
                             break;
@@ -74,12 +73,12 @@ public class UMERApp{
      */
     private static void menu(){
 
-        if(um.getUtilizador() == null)
-            running_menu_comprador();
+        if(um.getUtilizadorC() == null)
+            running_menu_cliente();
         else{
-            Utilizador util = um.getUtilizador();
+            Utilizador util = um.getUtilizadorC();
             if(util.getClass().getSimpleName().equals("Motorista"))
-                running_menu_vendedor();
+                running_menu_motorista();
             else running_menu_cliente();
         }
     }
@@ -280,17 +279,17 @@ public class UMERApp{
                         break;
                 case 6: sinalizaDisp();
                         break;
-                case 7: umer.fechaSessao();
+                case 7: um.fecharSessao();
             }*/
         }while(mMotorista.getOpcao() != 0);
     }
 
-    private static void carregaMenuME(){
+    private static void running_menu_motoristaEmp(){
         do{
-            mMotoristaEmp.executa();
-            /*
-            switch(mMotorista.getOpcao()){
-                case 1: adicionaVeiculo();
+            menu_motoristaEmp.executa();
+            
+            switch(menu_motoristaEmp.getOpcao()){
+                /*case 1: adicionaVeiculo();
                         break;
                 case 2: associaVeiculo();
                         break;
@@ -305,9 +304,9 @@ public class UMERApp{
                 case 7: sinalizaDisp();
                         break;
                 case 8: desassociaEmpresa();
-                        break;
-                case 9: umer.fechaSessao();
-            }*/
+                        break;*/
+                case 9: um.fecharSessao();
+            }
         }while(mMotoristaEmp.getOpcao() != 0);
     }
 
@@ -324,5 +323,51 @@ public class UMERApp{
         }while(mSolicitar.getOpcao() != 0);
     }
 
-
+    private static void registaViagem(){
+        Utilizador user = getUtilizadorC();
+        
+        Scanner is = new Scanner(System.in);
+        int classificacao, coordXinicial, coordYinicial, coordXfinal, coordYfinal;
+        double tempo, preco;
+        Calendar inicioT, fimT;
+        System.out.print("Localização inicial em X: \n");
+        coordXinicial = is.nextInt();
+        System.out.print("Localização inicial em Y: \n ");
+        coordYinicial = is.nextInt();
+        System.out.print("Localização final em X: \n");
+        coordXfinal = is.nextInt();
+        System.out.print("Localização final em Y: \n ");
+        coordYfinal = is.nextInt();
+        System.out.print("Classificação: \n ");
+        classificacao = is.nextInt();
+        System.out.print("Tempo de viagem: \n ");
+        tempo = is.nextDouble();
+        System.out.print("Preço da viagem: \n ");
+        preco = is.nextDouble();
+        System.out.print("Data do inicio da viagem: \n ");
+        inicioT = is.nextDouble();
+        System.out.print("Data do fim da viagem: \n ");
+        fimT = is.nextDouble();
+        /*ESTA CENA ESTA MAL*/ fimT = is.nextDouble();
+        
+        Localizacao inicio = Localizacao(coordXinicial, coordYinicial);
+        Localizacao fim = Localizacao(coordXfinal, coordYfinal);
+        Viagem v = Viagem(inicio, fim, classificacao, tempo, preco, inicioT, fimT);
+        
+        user.catViagens.add(v.clone());
+    }
+    
+    private static void sinalizaDisp(){
+        Utilizador user = getUtilizadorC();
+        String disponibilidade;
+        
+        Scanner is = new Scanner(System.in);
+        System.out.print("Disponivel? Sim ou não? \n ");
+        disponibilidade = is.nextLine;
+        
+        if(disponibilidade.equals("sim")) user.setDisponivel(true);
+        else user.setDisponivel(false);
+    }
+    
+    
 }
